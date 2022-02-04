276562d7564bb2e75d0a42e77d0f0f28b100bd81
Anderson Fernandes <andersonfferreira13@hotmail.com>
2022-02-04 20:41:28 -0300

    Add buttons update and delete

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -7,7 +7,7 @@
 
 
   <div class="py-12">
-      <x-back/>
+    <x-back />
 
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -54,6 +54,14 @@
               </tr>
             </tbody>
           </table>
+          <div class="flex flex-row gap-2 mt-4">
+            <button class="bg-blue-500 h-10 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+              Editar
+            </button>
+            <button class="bg-red-500 h-10 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded">
+              Excluir
+            </button>
+          </div>
         </div>
       </div>
     </div>
